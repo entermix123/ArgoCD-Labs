76e22d9aacc3d4f8a55dbb3737bcd2ed1725cfc4
entermix123 <balabanovjordan@gmail.com>
2025-12-22 13:29:59 +0200

    added resources for section 8 adding clusters & tracking strategies

---
+++ 08-argocd-add-cluster/add-cluster/add-cluster-using-terraform/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0.1"
+    }
+    argocd = {
+      source = "argoproj-labs/argocd"
+      version = "7.12.4"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path    = var.config_path
+  config_context = var.config_context
+}
+
+provider "argocd" {
+  alias       = "argocd_server"
+  server_addr = var.server_addr
+  username    = var.username
+  password    = var.password
+  insecure    = var.insecure
+}
